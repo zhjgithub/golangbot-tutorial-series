0374fbbe9ace61527f0257d890c60e350fd4aeba
organism <neuqzhj@live.com>
2018-01-26 22:57:12 +0800

    feature: Add init function check condition

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -3,10 +3,28 @@ package main
 import (
 	"fmt"
 	"golangbot-tutorial-series/geometry/rectangle" //importing custom package
+	"log"
 )
 
+/*
+ * 1. package variables
+ */
+var rectLen, rectWidth float64 = 6, 7
+
+/*
+*2. init function to check if length and width are greater than zero
+ */
+func init() {
+	println("main package initialized")
+	if rectLen < 0 {
+		log.Fatal("length is less than zero")
+	}
+	if rectWidth < 0 {
+		log.Fatal("width is less than zero")
+	}
+}
+
 func main() {
-	var rectLen, rectWidth float64 = 6, 7
 	fmt.Println("Geometrical shape properties")
 	/*Area function of rectangle package used
 	 */
